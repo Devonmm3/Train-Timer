3f788ea0455c8bea019a591a512d02aa2526e4f7
DevonMM <Devon.m.moran@gmail.com>
2018-12-10 18:18:55 -0500

    6th

--- assets/Javascript/app.js
+++ assets/Javascript/app.js
@@ -9,20 +9,52 @@ var config = {
     messagingSenderId: "54122609132"
 };
 firebase.initializeApp(config);
+var database = firebase.database();
+
+//need a button for adding the train
+$("#addTrainButton").on("click", function (event) {
+    event.preventDefault();
 
 
-var trainsName = $("#trainsNameInput").val().trim()
-var trainsLine = $("#trainsLineInput").val().trim()
-var Destination = $("#trainsDestinationInput").val().trim()
-var trainTimeInput = moment($("#trainTimeInput").val().trim(), "HH:mm").subtract(1, "years").format("");
-var trainFrequencyInput = $("#trainsFrequecyInput").val().trim();
+    var trainsName = $("#trainsNameInput").val().trim();
+    var trainsLine = $("#trainsLineInput").val().trim();
+    var Destination = $("#trainsDestinationInput").val().trim();
+    var trainTimeInput = moment($("#trainTimeInput").val().trim(), "HH:mm").subtract(1, "years").format("");
+    var trainFrequencyInput = $("#trainsFrequecyInput").val().trim();
 
-var newTrain = {
-    name: trainsName,
-    line: trainsLine,
-    Destination: Destination,
-    start: trainTimeInput,
-    frequency: trainFrequencyInput
+    var newTrain = {
+        name: trainsName,
+        line: trainsLine,
+        Destination: Destination,
+        start: trainTimeInput,
+        frequency: trainFrequencyInput
 
 
-};
+    };
+
+    database.ref().push(newTrain);
+
+    $("#trainsNameInput").val("");
+    $("#trainsLineInput").val("");
+    $("#trainsDestinationInput").val("");
+    $("#trainTimeInput").val("");
+    $("#trainFrequencyInput").val("");
+
+    return false;
+});
+
+//need a firebase add of the train to the database that then reflects in the HTML
+database.ref().on("child_added", function (childSnapshot, previousChildKey) {
+    console.log(childSnapshot.val());
+
+    var FirebaseTrainsName = childSnapshot.val().name;
+    var FirebaseTrainsLine = childSnapshot.val().Line;
+    var FirebaseDestination = childSnapshot.val().Destination;
+    var firebaseTrainTimeInput = childSnapshot.val().start;
+    var firebaseTrainFrequency = childSnapshot.val().frequency;
+
+    var TimeDiffernce = moment().diff(moment.unix(firebaseTrainTimeInput), "minutes");
+    var remainder = moment().deff(moment.unix(firebaseTrainTimeInput), "minutes") % firebaseTrainFrequency;
+
+
+})
